76cbb9b73a3930a94b9b055ebb7e7cc5d12ab7e2
Denis Sanchez <desanchezca@gmail.com>
2023-11-18 18:38:51 -0500

    Remove log

--- api/src/middlewares/secure.js
+++ api/src/middlewares/secure.js
@@ -2,7 +2,6 @@ import createError from "http-errors";
 
 
 export function isAuthenticated(req, res, next) {
-    console.log(req.isAuthenticated())
     if (req.isAuthenticated()) {
         next();
     } else {
