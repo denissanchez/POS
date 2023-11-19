4385ef0d6f6ebe4ba184b2d4550c72052ebad003
Denis Sanchez <desanchezca@gmail.com>
2023-11-19 13:12:19 -0500

    Add style for summary and create class for transaction

--- front/src/app/pos/summary/summary.component.ts
+++ front/src/app/pos/summary/summary.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ['./summary.component.scss'],
 })
 export class SummaryComponent implements OnInit {
-    items: number[] = [1, 2, 3, 4];
+    items: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
 
     ngOnInit(): void {
     }
---
+++ front/src/app/shared/models/transaction.ts
@@ -0,0 +1,49 @@
+import { Product } from "./product";
+
+
+function round(result: number) {
+    return Math.round((result + Number.EPSILON) * 100) / 100
+}
+
+
+export class Item {
+    public get subtotal() {
+        const base = this.product.price * this.quantity;
+
+        if (!isNaN(+this.discount) && this.discount > 0) {
+            return round(base - (base * this.discount / 100))
+        }
+        return round(base)
+    }
+
+    public get subtotalDiscounted() {
+        const base = this.product.price * this.quantity;
+
+        if (!isNaN(+this.discount) && this.discount > 0) {
+            return round(base * this.discount / 100);
+        }
+
+        return round(0)
+    }
+
+    constructor(
+        public product: Product = new Product(),
+        public quantity: number = 1,
+        public discount: number = 1,
+    ) {}
+}
+
+
+export class DraftTransaction {
+    public get total() {
+        return this.items.reduce((acc, curr) => curr.subtotal + acc, 0)
+    }
+
+    public get totalDiscounted() {
+        return this.items.reduce((acc, curr) => curr.subtotalDiscounted + acc, 0)
+    }
+
+    constructor(
+        public items: Item[],
+    ) {}
+}
