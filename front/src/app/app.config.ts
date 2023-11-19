2fb628d1e5fe1fd3f6e9752e8cdd9400e4400568
Denis Sanchez <desanchezca@gmail.com>
2023-11-19 13:35:10 -0500

    Adding sweetalert

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,8 +1,9 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(SweetAlert2Module.forRoot())]
 };
--- front/src/app/shared/shared.module.ts
+++ front/src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { SweetAlert2Module } from "@sweetalert2/ngx-sweetalert2";
 
 @NgModule({
     declarations: [],
@@ -9,11 +10,13 @@ import { FormsModule } from "@angular/forms";
         CommonModule,
         HttpClientModule,
         FormsModule,
+        SweetAlert2Module
     ],
     exports: [
         CommonModule,
         HttpClientModule,
         FormsModule,
+        SweetAlert2Module
     ],
 })
 export class SharedModule {
